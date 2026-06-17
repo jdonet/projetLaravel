6817b0094f701e09e73c7a0d53ee5dc712b6086d
Julien <juliendonet@gmail.com>
2026-06-17 13:24:44 -0400

    Commentaire

--- config/app.php
+++ config/app.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application Name
+    | Application Name : projetLaravel
     |--------------------------------------------------------------------------
     |
     | This value is the name of your application, which will be used when the
